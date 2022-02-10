3c9bb79acb48eb0232680bc37d644340b3be414f
jlbuild <juliabuildbot@gmail.com>
2022-02-10 22:22:45 +0000

    LAMMPS_jll build 1.0.1+2

--- src/wrappers/armv7l-linux-gnueabihf-libgfortran5-cxx11.jl
+++ src/wrappers/armv7l-linux-gnueabihf-libgfortran5-cxx11.jl
@@ -3,12 +3,11 @@ export liblammps, lmp
 
 using CompilerSupportLibraries_jll
 using MPItrampoline_jll
-using MicrosoftMPI_jll
 JLLWrappers.@generate_wrapper_header("LAMMPS")
 JLLWrappers.@declare_library_product(liblammps, "liblammps.so.0")
 JLLWrappers.@declare_executable_product(lmp)
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, MPItrampoline_jll, MicrosoftMPI_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, MPItrampoline_jll)
     JLLWrappers.@init_library_product(
         liblammps,
         "lib/liblammps.so",
--- src/wrappers/x86_64-w64-mingw32-libgfortran5-cxx03.jl
+++ src/wrappers/x86_64-w64-mingw32-libgfortran5-cxx03.jl
@@ -2,13 +2,12 @@
 export liblammps, lmp
 
 using CompilerSupportLibraries_jll
-using MPItrampoline_jll
 using MicrosoftMPI_jll
 JLLWrappers.@generate_wrapper_header("LAMMPS")
 JLLWrappers.@declare_library_product(liblammps, "liblammps.dll")
 JLLWrappers.@declare_executable_product(lmp)
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, MPItrampoline_jll, MicrosoftMPI_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, MicrosoftMPI_jll)
     JLLWrappers.@init_library_product(
         liblammps,
         "bin\\liblammps.dll",
